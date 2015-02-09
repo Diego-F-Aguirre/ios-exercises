d20cceb5938868f806ccc516cd8b7184ac85cd3f
Diego Aguirre <diego3884@gmail.com>
2015-02-09 09:47:03 -0500

    Storyboarding Alcolator 2

---
+++ Alcolator/Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Diego Aguirre on 2/9/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
